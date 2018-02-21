c678e1964603f23343e8665c717e2a174a17ade4
Alec <alecalegria@gmail.com>
2018-02-21 01:31:33 -0600

    forgot the routes update LOL

--- routes.py
+++ routes.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from models import db, User
-from forms import SignupForm
-
+from forms import SignupForm, LoginForm
 
 app = Flask(__name__)
 
@@ -20,18 +19,63 @@ def about():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if 'email' in session:
+        return redirect(url_for('home'))
+
     form = SignupForm()
     if request.method == 'POST':
         if form.validate() == False:
-            return render_template("signup.html", form=form)
+            form_error_input = "form-error--textInput"
+            return render_template("signup.html", form=form, form_error_input=form_error_input)
         else:
             newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
             db.session.add(newuser)
             db.session.commit()
-            return "It worked!"
+            session['email'] = newuser.email
+            return redirect(url_for('home'))
 
     elif request.method == "GET":
         return render_template('signup.html', form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if 'email' in session:
+        return redirect(url_for('home'))
+
+    form = LoginForm()
+
+    if request.method == "POST":
+        if form.validate() == False:
+            form_error_input = "form-error--textInput"
+            return render_template('login.html', form=form, form_error_input=form_error_input)
+        else:
+            email = form.email.data
+            password = form.password.data
+
+            user = User.query.filter_by(email=email).first()
+            if user is not None and user.check_password(password):
+                session['email'] = form.email.data
+                session['firstname'] = user.firstname
+                session['lastname'] = user.lastname
+                return redirect(url_for('home'))
+            else:
+                return redirect(url_for('login'))
+
+    elif request.method == "GET":
+        return render_template('login.html', form=form)
+
+@app.route("/logout")
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('index'))
+
+@app.route("/home")
+def home():
+    if 'email' not in session:
+        return redirect(url_for('login'))
+
+    return render_template("home.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
